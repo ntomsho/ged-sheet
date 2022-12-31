3372a79ff13fc49e51016faa1abc9502964eccf4
Nick Tomsho <46005988+ntomsho@users.noreply.github.com>
2022-12-31 14:45:49 -0500

    Change rerolls to infinite for playtesting

--- src/ged/sheet_main.jsx
+++ src/ged/sheet_main.jsx
@@ -29,7 +29,7 @@ class CharSheet extends React.Component {
         } else {
             this.state = {
                 level: 1,
-                rerolls: 10,
+                rerolls: 10000,
                 charName: "",
                 background: null,
                 appearance: null,
